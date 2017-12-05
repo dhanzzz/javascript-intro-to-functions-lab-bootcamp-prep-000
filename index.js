e14374d7a7c3dac22366326f086e630380e2b4bc
dave hanz <davidhan82@gmail.com>
2017-12-05 21:34:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
-var lowercase = "I can't hear you!"
-lowercase.toLowerCase() === lowercase // true
-var mixedCase = "I love you, too."
-mixedCase.toLowerCase() === mixedCase // false
-mixedCase.toUpperCase() === mixedCase // false
-var uppercase = "YES INDEED!"
-uppercase.toUpperCase() === uppercase
-function sayHiToGrandma(lowercase) {
-return "I can't hear you!"
+var uppercase = 'hELLO'
+var lowercase = 'hello'
+var mixedCase = 'i love you, Grandma'
+function sayHiToGrandma(hello) {
+  return "I can\'t hear you!"
 }
-function sayHiToGrandma(uppercase) {
-return "YES INDEED!"
+function sayHiToGrandma(hELLO) {
+  return "YES INDEED!"
 }
-function sayHiToGrandma(mixedCase) {
+function sayHiToGrandma(i love you, Grandma.) {
 return "I love you, too"
 }
